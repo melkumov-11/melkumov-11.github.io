ebd28fd2107cd410c6002de59ef7c3c4bd6bef2d
melkumov-11 <melkumov-11@yandex.ru>
2018-09-08 15:38:20 +0300

    Junsun

--- Junsun/js/slider.js
+++ Junsun/js/slider.js
@@ -6,6 +6,21 @@ $(document).ready(function(){
 		adaptiveHeight: true,
 		prevArrow: $('.arr'),
 		nextArrow: $('.arr-r'),
-		speed: 2000
+		speed: 2000,
+		responsive: [
+    	{
+      	breakpoint: 992,
+      	settings: {
+        	slidesToShow: 2
+      	}
+    	},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					arrows: false
+				}
+			}
+  	]
 	});
 });
