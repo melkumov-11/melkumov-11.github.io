268ee9e9a1bea3923b62f496e3d8aca4c81d3f40
melkumov-11 <melkumov-11@yandex.ru>
2019-07-28 00:48:17 +0300

    Начат новый раздел

--- construction_equipment/js/script.js
+++ construction_equipment/js/script.js
@@ -4,4 +4,21 @@ $(function() {
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
   });
+  
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 });
