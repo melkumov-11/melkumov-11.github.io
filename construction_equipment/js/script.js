674f6c7b252d1b7892f3790ea3f819d64177c223
melkumov-11 <melkumov-11@yandex.ru>
2019-07-30 03:13:07 +0300

    Коммит в 3:13 ночи

--- construction_equipment/js/script.js
+++ construction_equipment/js/script.js
@@ -5,23 +5,6 @@ $(function() {
     return false;
   });
   
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
-  
   $('.header-menu__hamburger').click(function(e) {
     e.preventDefault;
     this.toggleClass('header-menu__hamburger_active');
