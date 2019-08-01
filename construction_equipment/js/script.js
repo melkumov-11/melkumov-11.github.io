5fd1b62ad02054df897e0fae4e256defb7afc640
melkumov-11 <37487258+melkumov-11@users.noreply.github.com>
2019-08-01 15:43:58 +0300

    Страница "О нас"

--- construction_equipment/js/script.js
+++ construction_equipment/js/script.js
@@ -1,9 +1,9 @@
 $(function() {
-  $("a[href^='#']").click(function() {
-    var _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-    return false;
-  });
+//  $("a[href^='#']").click(function() {
+//    var _href = $(this).attr("href");
+//    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+//    return false;
+//  });
   
   $('.header-menu__hamburger').click(function() {
     $(this).toggleClass('header-menu__hamburger_active');
